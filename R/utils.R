ff071f12fbfabcec938a50462a80c356a62e8c1c
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-05-06 10:48:40 -0700

    cleanup

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,17 @@
 #' Generate mock coding/billing data frame
 #'
 #' @param rows number of rows to generate; default is 100
-#' @return A [tibble][tibble::tibble-package]
-#' @autoglobal
-#' @keyword internal
-#' @export
+#'
+#' @returns A [tibble][tibble::tibble-package]
+#'
 #' @examples
 #' generate_data(rows = 5)
+#'
+#' @autoglobal
+#'
+#' @export
 generate_data <- function(rows = 100){
-  x <- tibble::tibble(
+  x <- dplyr::tibble(
     claim_id = wakefield::id(n = rows),
     date_of_service = wakefield::date_stamp(n = rows,
                       start = lubridate::today() - lubridate::dyears(2),
@@ -19,10 +22,10 @@ generate_data <- function(rows = 100){
     ins_class = fixtuRes::set_vector(rows, set = c("Primary", "Secondary")),
     balance = wakefield::income(n = rows, digits = 2) / 300) |>
     dplyr::mutate(date_of_service = lubridate::as_date(date_of_service),
-    date_of_release = date_of_service + round(abs(rnorm(length(date_of_service), 11, 4))),
-    date_of_submission = date_of_release + round(abs(rnorm(length(date_of_release), 2, 2))),
-    date_of_acceptance = date_of_submission + round(abs(rnorm(length(date_of_submission), 3, 2))),
-    date_of_adjudication = date_of_acceptance + round(abs(rnorm(length(date_of_acceptance), 30, 3))))
+    date_of_release = date_of_service + round(abs(stats::rnorm(length(date_of_service), 11, 4))),
+    date_of_submission = date_of_release + round(abs(stats::rnorm(length(date_of_release), 2, 2))),
+    date_of_acceptance = date_of_submission + round(abs(stats::rnorm(length(date_of_submission), 3, 2))),
+    date_of_adjudication = date_of_acceptance + round(abs(stats::rnorm(length(date_of_acceptance), 30, 3))))
 
   x |> tidyr::nest(dates = tidyr::contains("date"))
 }
@@ -30,21 +33,34 @@ generate_data <- function(rows = 100){
 #' Count days between two dates
 #'
 #' @param df data frame
+#'
 #' @param start date column
+#'
 #' @param end date column
+#'
 #' @param name name of output column
-#' @return A [tibble][tibble::tibble-package]
-#' @autoglobal
-#' @keyword internal
-#' @export
+#'
+#' @returns A [tibble][tibble::tibble-package]
+#'
 #' @examples
 #' generate_data(rows = 5) |>
-#' tidyr::unnest(dates) |>
-#' count_days(date_of_service, date_of_release, provider_lag)
+#'   tidyr::unnest(dates) |>
+#'   count_days(date_of_service,
+#'              date_of_release,
+#'              provider_lag)
+#'
+#' @autoglobal
+#'
+#' @export
 count_days <- function(df, start, end, name) {
   df |>
-    dplyr::mutate({{ name }} := clock::date_count_between({{ start }},
-                                {{ end }}, "day"), .after = {{ end }})
+    dplyr::mutate(
+      {{ name }} := clock::date_count_between(
+        {{ start }},
+        {{ end }},
+        "day"),
+      .after = {{ end }}
+      )
 }
 
 #' Calculate Number of Days Between Two Dates
@@ -52,14 +68,18 @@ count_days <- function(df, start, end, name) {
 #' @note This calculation includes the end date in the sum (see example)
 #'
 #' @param df data frame containing date columns
+#'
 #' @param start column containing date(s) prior to end_date column
+#'
 #' @param end column containing date(s) after start_date column
+#'
 #' @param colname desired column name of output; default is "age"
-#' @return A [tibble][tibble::tibble-package] with a named column
+#'
+#' @returns A [tibble][tibble::tibble-package] with a named column
 #'    containing the calculated number of days.
 #'
 #' @examples
-#' date_ex <- tibble::tibble(x = seq.Date(as.Date("2021-01-01"),
+#' date_ex <- dplyr::tibble(x = seq.Date(as.Date("2021-01-01"),
 #'                           by = "month", length.out = 3),
 #'                           y = seq.Date(as.Date("2022-01-01"),
 #'                           by = "month", length.out = 3))
@@ -82,8 +102,9 @@ count_days <- function(df, start, end, name) {
 #' age_days(x, y, "days_between_x_y") |>
 #' age_days(x, lubridate::today(), "days_since_x") |>
 #' age_days(y, lubridate::today(), colname = "days_since_y")
+#'
 #' @autoglobal
-#' @keyword internal
+#'
 #' @export
 age_days <- function(df,
                      start,
@@ -92,11 +113,12 @@ age_days <- function(df,
 
   stopifnot(inherits(df, "data.frame"))
 
-  results <- df |>
-    dplyr::mutate(start = as.Date({{ start }}, "%yyyy-%mm-%dd", tz = "EST"),
-                  end = as.Date({{ end }}, "%yyyy-%mm-%dd", tz = "EST")) |>
-    dplyr::mutate("{colname}" := ((as.numeric(lubridate::days(end) - lubridate::days(start), "hours") / 24) + 1)) |>
+  df |>
+    dplyr::mutate(
+      start = as.Date({{ start }}, "%yyyy-%mm-%dd", tz = "EST"),
+      end = as.Date({{ end }}, "%yyyy-%mm-%dd", tz = "EST")
+      ) |>
+    dplyr::mutate(
+      "{colname}" := ((as.numeric(lubridate::days(end) - lubridate::days(start), "hours") / 24) + 1)) |>
     dplyr::select(!c(end, start))
-
-  return(results)
 }
---
+++ data-raw/net_pred_lirov.R
@@ -0,0 +1,147 @@
+# https://docs.google.com/spreadsheets/d/1braeEyikhzCMGc9IfNhnBGqqKOTVSU7J/edit#gid=976015322
+
+dplyr::tribble(
+  ~date,        ~gross_charges, ~net_payment, ~pct_ar_gt_120_days,
+  "2022-01-01", 180000,         77400,         0.21,
+  "2022-02-01", 210000,         77700,         0.17,
+  "2022-03-01", 177000,         95000,         0.15,
+  "2022-04-01", 195000,         97000,         0.01
+) |>
+  dplyr::mutate(
+    date = lubridate::ymd(date),
+    pct_ar_lte_120_days = 1 - pct_ar_gt_120_days,
+    pct_paid = net_payment / gross_charges,
+    net_pred = (gross_charges * pct_paid) * pct_ar_lte_120_days,
+    net_lag = dplyr::lag(net_payment, order_by = date))
+
+
+net_pred <- function(gct_prev,
+                     gct_pres,
+                     net_prev,
+                     parbx_pres) {
+
+  # What percentage of last month's charges paid
+  net_pct <- net_prev/gct_prev
+
+  # What percentage of last month's AR is <= 120 days
+  parbx_opp <- 1 - parbx_pres
+
+  # Net prediction
+  net_pred <- (gct_pres * net_pct) * parbx_opp
+
+  return(net_pred)
+
+}
+
+net_pred(gct_prev = 180000,
+         gct_pres = 210000,
+         net_prev = 77400,
+         parbx_pres = 0.21)
+
+
+
+# Needs to be lagged to the next month
+dplyr::tibble(
+  date = lubridate::ymd("2024-01-01") + months(0:3),
+  gross_charges = as.numeric(c(180000, 210000, 177000, 195000)),
+  net_payment = as.numeric(c(77400, 77700, 95000, 97000)),
+  parbx120 = as.numeric(c(0.21, 0.17, 0.15, 0.1))
+)
+
+net_pred_month <- function(df, gct, net, parbx) {
+
+  stopifnot(inherits(df, "data.frame"))
+
+  results <- df |> dplyr::mutate(net_pct = {{ net }} / {{ gct }},
+                                 parbx_rev = 1 - {{ parbx }}
+  )
+
+  # Net prediction
+  net_pred <- (gct_pres * pct_paid) * parbx_opp
+
+  return(net_pred)
+
+}
+
+
+dplyr::tibble(
+  date = (c(seq(
+    as.Date("2024-01-01"),
+    by = "month",
+    length.out = 12
+  ))
+  ),
+  gross_charges = c(
+    325982, 297731.74, 198655.14,
+    186047, 123654, 131440.28,
+    153991, 156975, 146878.12,
+    163799.44, 151410.74, 169094.46
+  ),
+  ending_ar = c(
+    288432.52, 307871.08, 253976.56,
+    183684.90, 204227.59, 203460.47,
+    182771.32, 169633.64, 179347.72,
+    178051.11, 162757.49, 199849.30
+  ),
+  net_payment = c(
+    104181.64, 124548.88, 119445.53,
+    71756.18, 50112.23, 65715.41,
+    85245.91, 68055.25, 73669.21,
+    81422.37, 78436.27, 69030.83
+  ),
+  adjustments = c(
+    170173.76, 153744.3, 133104.13,
+    84582.48, 52999.08, 66491.99,
+    89434.24, 102057.43, 63494.83,
+    83673.68, 88268.09, 62971.82
+  ),
+  pos = c(
+    16012.80, 16304.75, 10844.50,
+    1824.07, 6240.95, 7376.63,
+    9155.36, 9740.75, 8602.64,
+    8599.35, 7348.15, 10461.59
+  ),
+  avg_days_to_bill = c(
+    5.33, 8.08, 6.07,
+    3.76, 2.61, 2.77,
+    3.43, 3.36, 2.54,
+    2.63, 3.26, 3.4
+  ),
+  encounters = c(
+    1568, 1473, 1031,
+    553, 713, 723,
+    813, 798, 787,
+    851, 762, 834
+  ),
+  unique_patients = c(
+    1204, 1162, 758,
+    428, 609, 578,
+    636, 658, 624,
+    702, 565, 670
+  ),
+  new_patients = c(
+    129, 120, 61,
+    32, 123, 77,
+    93, 76, 65,
+    61, 61, 95
+  ),
+  em_visits = c(
+    1184, 1130, 813,
+    427, 550, 572,
+    599, 615, 597,
+    617, 487, 662
+  ),
+  rvus = c(
+    1564.5, 1474.35, 995.6,
+    517.34, 739.5, 754.64,
+    863.41, 835.53, 826.4,
+    875.49, 814.78, 911.65
+  )
+) |>
+  dplyr::mutate(
+    earb_abs = ending_ar - dplyr::lag(ending_ar, order_by = date),
+    earb_rel = earb_abs / dplyr::lag(ending_ar, order_by = date),
+    earb_abs = dplyr::coalesce(earb_abs, 0),
+    earb_rel = dplyr::coalesce(earb_rel, 0),
+    earb_rel = janitor::round_half_up(earb_rel, digits = 5)
+  )
