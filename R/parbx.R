22a07a095d45de982d0aac56b828866f0fa73137
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-05-28 16:41:43 -0700

    parbx

--- R/parbx.R
+++ R/parbx.R
@@ -1,59 +1,19 @@
+#' Generate Mock PARBx data
 #' @keywords internal
 #'
-#' @noRd
-parbx_ex <- function() {
+#' @examples
+#' # Every name generates 60 rows of data
+#' parbx_ex(
+#'   c("Cigna", "BCBS", "Anthem",
+#'     "Humana", "UHC", "Medicare",
+#'     "Coventry", "Medicaid", "Centene",
+#'     "CVSHealth"))
+#'
+#'
+#' @export
+parbx_ex <- function(payer, ...) {
 
-  cigna <- dplyr::tibble(
-    date = vctrs::vec_rep_each(clock::date_build(2024, 1:12, invalid = "previous"), times = 5),
-    month = clock::date_month_factor(date),
-    payer = "Cigna",
-    aging_bin = vctrs::vec_rep(c("0-30", "31-60", "61-90", "91-120", "121+"), times = 12)) |>
-    dplyr::group_by(month) |>
-    dplyr::mutate(aging_prop = vctrs::vec_rep(wakefield::probs(5), times = 1)) |>
-    dplyr::ungroup()
-
-  humana <- dplyr::tibble(
-    date = vctrs::vec_rep_each(clock::date_build(2024, 1:12, invalid = "previous"), times = 5),
-    month = clock::date_month_factor(date),
-    payer = "Humana",
-    aging_bin = vctrs::vec_rep(c("0-30", "31-60", "61-90", "91-120", "121+"), times = 12)) |>
-    dplyr::group_by(month) |>
-    dplyr::mutate(aging_prop = vctrs::vec_rep(wakefield::probs(5), times = 1)) |>
-    dplyr::ungroup()
-
-  uhc <- dplyr::tibble(
-    date = vctrs::vec_rep_each(clock::date_build(2024, 1:12, invalid = "previous"), times = 5),
-    month = clock::date_month_factor(date),
-    payer = "UHC",
-    aging_bin = vctrs::vec_rep(c("0-30", "31-60", "61-90", "91-120", "121+"), times = 12)) |>
-    dplyr::group_by(month) |>
-    dplyr::mutate(aging_prop = vctrs::vec_rep(wakefield::probs(5), times = 1)) |>
-    dplyr::ungroup()
-
-  anthem <- dplyr::tibble(
-    date = vctrs::vec_rep_each(clock::date_build(2024, 1:12, invalid = "previous"), times = 5),
-    month = clock::date_month_factor(date),
-    payer = "Anthem",
-    aging_bin = vctrs::vec_rep(c("0-30", "31-60", "61-90", "91-120", "121+"), times = 12)) |>
-    dplyr::group_by(month) |>
-    dplyr::mutate(aging_prop = vctrs::vec_rep(wakefield::probs(5), times = 1)) |>
-    dplyr::ungroup()
-
-  bcbcs <- dplyr::tibble(
-    date = vctrs::vec_rep_each(clock::date_build(2024, 1:12, invalid = "previous"), times = 5),
-    month = clock::date_month_factor(date),
-    payer = "BCBS",
-    aging_bin = vctrs::vec_rep(c("0-30", "31-60", "61-90", "91-120", "121+"), times = 12)) |>
-    dplyr::group_by(month) |>
-    dplyr::mutate(aging_prop = vctrs::vec_rep(wakefield::probs(5), times = 1)) |>
-    dplyr::ungroup()
-
-  vctrs::vec_rbind(
-    cigna,
-    humana,
-    uhc,
-    anthem,
-    bcbcs) |>
+  purrr::map_dfr(payer, parbx_ex_) |>
     dplyr::mutate(
       aging_bin = suppressWarnings(factor(
         aging_bin,
@@ -62,4 +22,19 @@ parbx_ex <- function() {
       )
     ) |>
     .add_class()
+}
+
+#' @keywords internal
+#'
+#' @noRd
+parbx_ex_ <- function(payer, ...) {
+
+  dplyr::tibble(
+    date = vctrs::vec_rep_each(clock::date_build(2024, 1:12, invalid = "previous"), times = 5),
+    month = clock::date_month_factor(date),
+    payer = payer,
+    aging_bin = vctrs::vec_rep(c("0-30", "31-60", "61-90", "91-120", "121+"), times = 12)) |>
+    dplyr::group_by(month) |>
+    dplyr::mutate(aging_prop = vctrs::vec_rep(wakefield::probs(5), times = 1)) |>
+    dplyr::ungroup()
 }
